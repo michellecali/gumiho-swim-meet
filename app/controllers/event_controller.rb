3229631d866efc9cbe56613d6dd9212070397f64
Blair Englund <blair.englund@gmail.com>
2016-07-13 13:49:33 -0500

    created view controls for each event

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,3 +1,51 @@
 MyApp.before "/*" do
 	"Hello World"
 end
+
+MyApp.get "events/1650freestyle" do
+	erb :"events/event"
+end
+
+MyApp.get "events/200freestyle" do
+	erb :"events/event"
+end
+
+MyApp.get "events/100backstroke" do
+	erb :"events/event"
+end
+
+MyApp.get "events/100breaststroke" do
+	erb :"events/event"
+end
+
+MyApp.get "events/200butterfly" do
+	erb :"events/event"
+end
+
+MyApp.get "events/50freestyle" do
+	erb :"events/event"
+end
+
+MyApp.get "events/100freestyle" do
+	erb :"events/event"
+end
+
+MyApp.get "events/200backstroke" do
+	erb :"events/event"
+end
+
+MyApp.get "events/200breaststroke" do
+	erb :"events/event"
+end
+
+MyApp.get "events/500Freestyle" do
+	erb :"events/event"
+end
+
+MyApp.get "events/100butterfly" do
+	erb :"events/event"
+end
+
+MyApp.get "events/400medley" do
+	erb :"events/event"
+end
