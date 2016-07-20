552cdbc5313c1059219158f476476d29f4e1a80b
Blair Englund <blair.englund@gmail.com>
2016-07-20 13:20:17 -0500

    transitions not working on medalists

--- public/winners_scripts.js
+++ public/winners_scripts.js
@@ -9,6 +9,14 @@ window.addEventListener('load', function() {
 
 	//function to change views (from 0%height to full height)
 	var toggleview = function() {
+		//objects to click (header for each event)
+		var event100BS = document.getElementById('100BS');
+
+		//objects whose view will be changed (all the individual placers)
+		var gold100BS = document.getElementsByClassName('winners__listitem--gold');
+		var silver100BS = document.getElementsByClassName('winners__listitem--silver');
+		var bronze100BS = document.getElementsByClassName('winners__listitem--bronze');
+
 		if (gold100BS[0].style.height == "0px") {
 			gold100BS[0].style.height = 'auto';
 			silver100BS[0].style.height = 'auto';
@@ -19,8 +27,9 @@ window.addEventListener('load', function() {
 			silver100BS[0].style.height = '0px';
 			bronze100BS[0].style.height = '0px';
 		}
+		event100BS.removeEventListener('mousedown', toggleview);
 	};
-
-	event100BS.addEventListener('click', toggleview)
+	event100BS.addEventListener('mousedown', toggleview) //without this it won't fire the first time, gets removed by function
+	event100BS.addEventListener('mouseup', toggleview)
 
 });
