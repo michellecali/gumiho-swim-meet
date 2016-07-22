4095b17a623e8bb03d816db9e198bcd50aad6d5d
Blair Englund <blair.englund@gmail.com>
2016-07-22 10:25:12 -0500

    winners dropdown code DRYed up: next step: integrate an up/down arrow that corresponds with displayed/hidden lists

--- public/winners_scripts.js
+++ public/winners_scripts.js
@@ -5,60 +5,33 @@ window.addEventListener('load', function() {
 	//objects whose view will be changed (all the individual placers)
 	var medalists = document.querySelectorAll('ol.winners__list')
 
+	//hides all of the medalists to start
 	for (var i = medalists.length - 1; i >= 0; i--) {
 		medalists[i].style.height = "0px";
 	}
 
-	function toggleview(x) {
-		if (medalists[x].style.height == "0px") {
-			medalists[x].style.height = 'auto';
-		}
-		else {
-			medalists[x].style.height = '0px';
-		}
-	}	
+	//total number of events
+	var eventnum = eventheads.length;
 
-	//for (var i = eventheads.length - 1; i >= 0; i--) {
+	//function to generate an event listener on each header
+	//
+	//x is the position of a header in eventhead and the corresponding list of medalists
+	function makeEventListener(x){
+		eventheads[x].addEventListener('mouseup', function(){
+			//if the list is hidden, it will be displayed
+			if (medalists[x].style.height == "0px") {
+				medalists[x].style.height = 'auto';
+			}
+			//if the list is not hidded, clicking on the header will hide it again
+			else {
+				medalists[x].style.height = '0px';
+			}
+		})
+	}
 
-		eventheads[0].addEventListener('mouseup', function(){
-			if (medalists[0].style.height == "0px") {
-				medalists[0].style.height = 'auto';
-			}
-			else {
-				medalists[0].style.height = '0px';
-			}
-		})
-		eventheads[1].addEventListener('mouseup', function(){
-			if (medalists[1].style.height == "0px") {
-				medalists[1].style.height = 'auto';
-			}
-			else {
-				medalists[1].style.height = '0px';
-			}
-		})
-		eventheads[2].addEventListener('mouseup', function(){
-			if (medalists[2].style.height == "0px") {
-				medalists[2].style.height = 'auto';
-			}
-			else {
-				medalists[2].style.height = '0px';
-			}
-		})
-		eventheads[3].addEventListener('mouseup', function(){
-			if (medalists[3].style.height == "0px") {
-				medalists[3].style.height = 'auto';
-			}
-			else {
-				medalists[3].style.height = '0px';
-			}
-		})
-		eventheads[4].addEventListener('mouseup', function(){
-			if (medalists[4].style.height == "0px") {
-				medalists[4].style.height = 'auto';
-			}
-			else {
-				medalists[4].style.height = '0px';
-			}
-		})
+	//create event listeners for the total number of headers
+	for (var i = 0; i < eventnum; i++) {
+		makeEventListener(i);
+	}
 
 });
